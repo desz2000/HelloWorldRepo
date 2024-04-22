fdb9dfdff5de49cb7eb365d63ea8ea0876cc2ec5
desz2000 <115717049+desz2000@users.noreply.github.com>
2024-04-21 22:17:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-﻿namespace HeloWorld;
+namespace HelloWorld;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+        Console.WriteLine("Hello, World!");
     }
 }
